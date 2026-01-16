f45531545e9c04719ee1cf024b49646c4b112b04
ashlyaud <maksimknutov90@gmail.com>
2026-01-16 05:16:24 +0300

    additional data has been added for output

--- userservice/internal/transport/rest/server.go
+++ userservice/internal/transport/rest/server.go
@@ -24,7 +24,7 @@ func (r *RestServer) MustStart() {
 
 	if err := r.serv.ListenAndServe(); err != nil {
 		if err != http.ErrServerClosed {
-			panic("server bad start")
+			panic("server bad start: " + err.Error())
 		}
 	}
 }
