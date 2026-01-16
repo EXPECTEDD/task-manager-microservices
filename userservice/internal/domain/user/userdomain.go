93a125b32fc2706e47809be0d802578129ec069e
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-17 00:09:03 +0300

    changed from password to hashPassword

--- userservice/internal/domain/user/userdomain.go
+++ userservice/internal/domain/user/userdomain.go
@@ -1,19 +1,19 @@
 package userdomain
 
 type UserDomain struct {
-	FirstName  string
-	MiddleName string
-	LastName   string
-	Password   string
-	Email      string
+	FirstName    string
+	MiddleName   string
+	LastName     string
+	HashPassword string
+	Email        string
 }
 
-func NewUserDomain(firstname, middlename, lastname, password, email string) *UserDomain {
+func NewUserDomain(firstname, middlename, lastname, hashPassword, email string) *UserDomain {
 	return &UserDomain{
-		FirstName:  firstname,
-		MiddleName: middlename,
-		LastName:   lastname,
-		Password:   password,
-		Email:      email,
+		FirstName:    firstname,
+		MiddleName:   middlename,
+		LastName:     lastname,
+		HashPassword: hashPassword,
+		Email:        email,
 	}
 }
