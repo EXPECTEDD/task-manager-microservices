a60215d5d60b0e865f17261b1c1036f124bc263e
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-16 21:55:32 +0300

    added user domain

---
+++ userservice/internal/domain/user/userdomain.go
@@ -0,0 +1,19 @@
+package userdomain
+
+type UserDomain struct {
+	FirstName string
+	MiddleName string
+	LastName string
+	Password string
+	Email string
+}
+
+func NewUserDomain(firstname, middlename, lastname, password, email string) *UserDomain {
+	return &UserDomain{
+		FirstName: firstname,
+		MiddleName: middlename,
+		LastName: lastname,
+		Password: password,
+		Email: email,
+	}
+}
