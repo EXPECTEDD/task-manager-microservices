e9c85f87ce1ea4f1fe0af994db73193eb1e0bf4b
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-25 22:36:47 +0300

    chore: added logs

--- projectservice/internal/transport/rest/middleware/get_session.go
+++ projectservice/internal/transport/rest/middleware/get_session.go
@@ -1,15 +1,18 @@
 package middleware
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func GetSessionMiddleware() gin.HandlerFunc {
+func GetSessionMiddleware(log *slog.Logger) gin.HandlerFunc {
+	const op = "middleware.GetSessionMiddleware"
 	return func(ctx *gin.Context) {
 		cookie, err := ctx.Cookie("sessionId")
 		if err != nil {
+			log.Info("a request arrived without a sessionId", slog.String("op", op))
 			ctx.JSON(http.StatusUnauthorized, gin.H{
 				"error": "needed cookie with sessionId",
 			})
