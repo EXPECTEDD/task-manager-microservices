961bdaae6b8e2da5704ceea2a16394c54aee2c21
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-26 21:02:29 +0300

    chore: added constraint

--- projectservice/migrations/001_create_table_projects.up.sql
+++ projectservice/migrations/001_create_table_projects.up.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS projects(
     id SERIAL PRIMARY KEY,
     owner_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    CONSTRAINT unique_owner_id UNIQUE (owner_id, name)
 );
 
 CREATE INDEX idx_projects_owner_id ON projects(owner_id);
