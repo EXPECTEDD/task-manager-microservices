ac8015483c37f95f193a77c87f48e99a9784be5b
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-01 00:07:38 +0300

    feat: added implementation for get all handl

--- projectservice/internal/transport/rest/handler/resthandler.go
+++ projectservice/internal/transport/rest/handler/resthandler.go
@@ -11,7 +11,9 @@ import (
 	handlvalidator "projectservice/internal/transport/rest/handler/validator"
 	createerr "projectservice/internal/usecase/error/createproject"
 	deleteerr "projectservice/internal/usecase/error/deleteproject"
+	getallerr "projectservice/internal/usecase/error/getallprojects"
 	"projectservice/internal/usecase/interfaces"
+	getallmodel "projectservice/internal/usecase/models/getallprojects"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,13 +23,20 @@ type RestHandler struct {
 
 	createProjUC interfaces.CreateProjectUsecase
 	deleteProjUC interfaces.DeleteProjectUsecase
+	getAllProjUC interfaces.GetAllProjectsUsecase
 }
 
-func NewHandler(log *slog.Logger, createProjUC interfaces.CreateProjectUsecase, deleteProjUC interfaces.DeleteProjectUsecase) *RestHandler {
+func NewHandler(
+	log *slog.Logger,
+	createProjUC interfaces.CreateProjectUsecase,
+	deleteProjUC interfaces.DeleteProjectUsecase,
+	getAllProjUC interfaces.GetAllProjectsUsecase,
+) *RestHandler {
 	return &RestHandler{
 		log:          log,
 		createProjUC: createProjUC,
 		deleteProjUC: deleteProjUC,
+		getAllProjUC: getAllProjUC,
 	}
 }
 
@@ -44,7 +53,7 @@ func (h *RestHandler) Create(ctx *gin.Context) {
 
 	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
 
-	log.Info("start create request")
+	log.Info("starting create request")
 
 	var req *createdto.CreateRequest
 
@@ -109,7 +118,7 @@ func (h *RestHandler) Delete(ctx *gin.Context) {
 
 	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
 
-	log.Info("start delete request")
+	log.Info("starting delete request")
 
 	var req *deletedto.DeleteRequest
 
@@ -162,7 +171,41 @@ func (h *RestHandler) Delete(ctx *gin.Context) {
 }
 
 func (h *RestHandler) GetAll(ctx *gin.Context) {
-	panic("not implemented")
+	const op = "resthandler.GetAll"
+
+	userId := getUserId(ctx)
+	if userId == 0 {
+		h.log.Error("failed to get userId", slog.String("op", op))
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal server error",
+		})
+	}
+
+	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
+
+	log.Info("starting get all request")
+
+	in := getallmodel.NewGetAllProjectsInput(userId)
+
+	out, err := h.getAllProjUC.Execute(ctx, in)
+	if err != nil {
+		if errors.Is(err, getallerr.ErrProjectsNotFound) {
+			log.Info("projects not found")
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			log.Warn("cannot get projects", slog.String("error", err.Error()))
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "internal server error",
+			})
+		}
+	}
+
+	log.Info("get all request completed successfully")
+
+	res := handlmapper.GetAllOutputToResponse(out)
+	ctx.JSON(http.StatusOK, res)
 }
 
 func getUserId(ctx *gin.Context) uint32 {
