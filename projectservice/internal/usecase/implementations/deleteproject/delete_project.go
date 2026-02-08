4b2b4a506d34bba6b0b9aa41d0f35df3a717b2e8
ashlyaud <maksimknutov90@gmail.com>
2026-02-08 04:28:23 +0300

    refactor: updated delete usecase implementation

--- projectservice/internal/usecase/implementations/deleteproject/delete_project.go
+++ projectservice/internal/usecase/implementations/deleteproject/delete_project.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"log/slog"
-	projectdomain "projectservice/internal/domain/project"
 	"projectservice/internal/repository/storage"
 	deleteerr "projectservice/internal/usecase/error/deleteproject"
 	deletemodel "projectservice/internal/usecase/models/deleteproject"
@@ -26,17 +25,15 @@ func NewDeleteProjectUC(log *slog.Logger, stor storage.Storage) *DeleteProjectUC
 func (d *DeleteProjectUC) Execute(ctx context.Context, in *deletemodel.DeleteProjectInput) (*deletemodel.DeleteProjectOutput, error) {
 	const op = "deleteproject.Execute"
 
-	log := d.log.With(slog.String("op", op), slog.Int("userId", int(in.OwnerId)))
+	log := d.log.With(slog.String("op", op), slog.Int("projectId", int(in.ProjectId)))
 
 	log.Info("starting delete project")
 
-	proj, err := projectdomain.NewProjectDomain(in.OwnerId, in.Name)
-	if err != nil {
-		log.Info("can't delete a project", slog.String("error", err.Error()))
-		return deletemodel.NewDeleteProjectOutput(false), err
+	if in.ProjectId == 0 {
+		return deletemodel.NewDeleteProjectOutput(false), deleteerr.ErrInvalidProjectId
 	}
 
-	err = d.stor.Delete(ctx, proj)
+	err := d.stor.Delete(ctx, in.ProjectId)
 	if err != nil {
 		if errors.Is(err, storage.ErrNotFound) {
 			log.Info("project not found")
