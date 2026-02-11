ca53acc9bc60502ef2398d242da8919bda3befcb
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-11 11:00:30 +0300

    fix: fix create querie

--- taskservice/internal/infrastructure/postgres/queries.go
+++ taskservice/internal/infrastructure/postgres/queries.go
@@ -1,5 +1,5 @@
 package postgres
 
 var (
-	QuerieCreate = "INSERT INTO tasks (project_id, description, deadlone) VALUES($1, $2, $3) RETURNING id;"
+	QuerieCreate = "INSERT INTO tasks (project_id, description, deadline) VALUES($1, $2, $3) RETURNING id;"
 )
