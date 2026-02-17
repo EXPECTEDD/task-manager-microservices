a17e04a4590024fd28f1b0bc377b15668f43e4c3
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-17 11:09:01 +0300

    feat: added implementation for update handl

--- projectservice/internal/transport/rest/handler/resthandler.go
+++ projectservice/internal/transport/rest/handler/resthandler.go
@@ -7,13 +7,16 @@ import (
 	projectdomain "projectservice/internal/domain/project"
 	createdto "projectservice/internal/transport/rest/handler/dto/create"
 	deletedto "projectservice/internal/transport/rest/handler/dto/delete"
+	updatedto "projectservice/internal/transport/rest/handler/dto/update"
 	handlmapper "projectservice/internal/transport/rest/handler/mapper"
 	handlvalidator "projectservice/internal/transport/rest/handler/validator"
 	createerr "projectservice/internal/usecase/error/createproject"
 	deleteerr "projectservice/internal/usecase/error/deleteproject"
 	getallerr "projectservice/internal/usecase/error/getallprojects"
+	updateerr "projectservice/internal/usecase/error/updateproject"
 	"projectservice/internal/usecase/interfaces"
 	getallmodel "projectservice/internal/usecase/models/getallprojects"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,6 +27,7 @@ type RestHandler struct {
 	createProjUC interfaces.CreateProjectUsecase
 	deleteProjUC interfaces.DeleteProjectUsecase
 	getAllProjUC interfaces.GetAllProjectsUsecase
+	updateProjUC interfaces.UpdateProjectUsecase
 }
 
 func NewHandler(
@@ -31,12 +35,14 @@ func NewHandler(
 	createProjUC interfaces.CreateProjectUsecase,
 	deleteProjUC interfaces.DeleteProjectUsecase,
 	getAllProjUC interfaces.GetAllProjectsUsecase,
+	updateProjUC interfaces.UpdateProjectUsecase,
 ) *RestHandler {
 	return &RestHandler{
 		log:          log,
 		createProjUC: createProjUC,
 		deleteProjUC: deleteProjUC,
 		getAllProjUC: getAllProjUC,
+		updateProjUC: updateProjUC,
 	}
 }
 
@@ -49,6 +55,7 @@ func (h *RestHandler) Create(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": "internal server error",
 		})
+		return
 	}
 
 	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
@@ -114,6 +121,7 @@ func (h *RestHandler) Delete(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": "internal server error",
 		})
+		return
 	}
 
 	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
@@ -174,6 +182,7 @@ func (h *RestHandler) GetAll(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": "internal server error",
 		})
+		return
 	}
 
 	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
@@ -201,6 +210,81 @@ func (h *RestHandler) GetAll(ctx *gin.Context) {
 	log.Info("get all request completed successfully")
 
 	res := handlmapper.GetAllOutputToResponse(out)
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (h *RestHandler) Update(ctx *gin.Context) {
+	const op = "resthandler.Update"
+
+	userId := getUserId(ctx)
+	if userId == 0 {
+		h.log.Error("failed to get userId", slog.String("op", op))
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal server error",
+		})
+		return
+	}
+
+	log := h.log.With(slog.String("op", op), slog.Int("userId", int(userId)))
+
+	projectIdVar := ctx.Param("project_id")
+	projectId, err := strconv.Atoi(projectIdVar)
+	if err != nil {
+		log.Error("project_id in context has invalid type")
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid project_id type",
+		})
+	}
+	if uint32(projectId) == 0 {
+		log.Error("invalid project_id")
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid project_id value",
+		})
+	}
+
+	log.Info("starting updated request")
+
+	var req *updatedto.UpdateRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		log.Warn("error with request data", slog.String("error", err.Error()))
+		errMap, ok := handlvalidator.MapValidationErrors(err)
+		if ok {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"errors": errMap,
+			})
+		} else {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "bad request body",
+			})
+		}
+		return
+	}
+
+	in := handlmapper.UpdateRequestToInput(userId, uint32(projectId), req)
+
+	out, err := h.updateProjUC.Execute(ctx.Request.Context(), in)
+	if err != nil {
+		if errors.Is(err, updateerr.ErrInvalidName) {
+			log.Info("invalid new project name")
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid new project name",
+			})
+		} else if errors.Is(err, updateerr.ErrProjectNotFound) {
+			log.Info("project not found")
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": "project not found",
+			})
+		} else if errors.Is(err, updateerr.ErrProjectNameAlreadyExists) {
+			log.Info("project name already exists")
+			ctx.JSON(http.StatusConflict, gin.H{
+				"error": "project name already exists",
+			})
+		}
+		return
+	}
+
+	log.Info("update request completed successfully")
+	res := handlmapper.UpdateOutputToResponse(out)
 	ctx.JSON(http.StatusOK, res)
 }
 
