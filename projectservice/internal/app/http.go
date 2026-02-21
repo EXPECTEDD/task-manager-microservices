ec84d633b6d508379e3ea5db192081aa02048e8e
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-21 15:20:26 +0300

    fix: fixed a bug where 200 OK was returned to a non-existent endpoint

--- projectservice/internal/app/http.go
+++ projectservice/internal/app/http.go
@@ -16,15 +16,18 @@ import (
 func mustLoadHttpServer(cfg *config.Config, log *slog.Logger, handl *resthandler.RestHandler, sessionValid sessionalidator.SessionValidator) *rest.RestServer {
 	gin.SetMode(cfg.RestConf.Mode)
 	router := gin.New()
-	router.Use(gin.Recovery())
-	router.Use(middleware.GetSessionMiddleware(log))
-	router.Use(middleware.SessionAuthMiddleware(log, sessionValid, cfg.ConnectionsConf.UserServConnConf.ResponseTimeout))
-	router.Use(middleware.TimeoutMiddleware(cfg.RestConf.RequestTimeout))
 
-	router.POST("/project/create", handl.Create)
-	router.DELETE("/project/delete/:project_id", handl.Delete)
-	router.GET("/project/getall", handl.GetAll)
-	router.PATCH("/project/update/:project_id", handl.Update)
+	group := router.Group("/")
+
+	group.Use(gin.Recovery())
+	group.Use(middleware.GetSessionMiddleware(log))
+	group.Use(middleware.SessionAuthMiddleware(log, sessionValid, cfg.ConnectionsConf.UserServConnConf.ResponseTimeout))
+	group.Use(middleware.TimeoutMiddleware(cfg.RestConf.RequestTimeout))
+
+	group.POST("/project/create", handl.Create)
+	group.DELETE("/project/delete/:project_id", handl.Delete)
+	group.GET("/project/getall", handl.GetAll)
+	group.PATCH("/project/update/:project_id", handl.Update)
 
 	serv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.RestConf.Port),
