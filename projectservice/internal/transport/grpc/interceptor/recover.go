1c5a650fcbbc91daf209534810441c30dd7c1e95
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-22 22:25:26 +0300

    feat: added recover interceptor

---
+++ projectservice/internal/transport/grpc/interceptor/recover.go
@@ -0,0 +1,29 @@
+package interceptor
+
+import (
+	"context"
+	"log/slog"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func RecoverInterceptor(log *slog.Logger) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (resp any, err error) {
+		defer func(){
+			if r := recover(); r != nil {
+				log.Error("request caused panic", slog.Any("panic", r))
+				resp = nil
+				err = status.Error(codes.Internal, "internal server error")
+			}
+		}()
+
+		return handler(ctx, req)
+	}
+}
