34a5a702cb42fcc929484182aa38c770c7a08171
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-22 22:34:50 +0300

    feat: added timeout interceptor

---
+++ projectservice/internal/transport/grpc/interceptor/timeout.go
@@ -0,0 +1,32 @@
+package interceptor
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TimeoutInterceptor(log *slog.Logger, timeout time.Duration) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (resp any, err error) {
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		defer func() {
+			if ctx.Err() != nil {
+				resp = nil
+				err = status.Error(codes.DeadlineExceeded, "request time out")
+			}
+		}()
+
+		return handler(ctx, req)
+	}
+}
