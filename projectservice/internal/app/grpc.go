22994e88d9bd7f31b1095ae9edbfe3fb8ca95f46
ashlyaud <maksimknutov.w80@gmail.com>
2026-02-22 22:37:19 +0300

    chore: added interceptors in grpc server

--- projectservice/internal/app/grpc.go
+++ projectservice/internal/app/grpc.go
@@ -4,13 +4,19 @@ import (
 	"log/slog"
 	"projectservice/internal/config"
 	grpcserver "projectservice/internal/transport/grpc"
+	"projectservice/internal/transport/grpc/interceptor"
 	projectservicev1 "projectservice/proto/projectservice"
 
 	"google.golang.org/grpc"
 )
 
 func mustLoadGRPCServer(log *slog.Logger, cfg *config.Config, handl projectservicev1.ProjectServiceServer) *grpcserver.GRPCServer {
-	serv := grpc.NewServer()
+	serv := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			interceptor.RecoverInterceptor(log),
+			interceptor.TimeoutInterceptor(log, cfg.GrpcConf.Timeout),
+		),
+	)
 
 	return grpcserver.NewGRPCServer(log, handl, serv, cfg.GrpcConf.Host, cfg.GrpcConf.Port)
 }
