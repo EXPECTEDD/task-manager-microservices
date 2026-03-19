1a2a69388fbf0729a21992e2ac89bfe6450f4b15
EXPECTEDD <maksimknutov.w80@gmail.com>
2026-03-20 02:51:16 +0300

    chore: added new queries

--- taskservice/internal/infrastructure/postgres/queries.go
+++ taskservice/internal/infrastructure/postgres/queries.go
@@ -1,5 +1,7 @@
 package postgres
 
 var (
-	QuerieCreate = "INSERT INTO tasks (project_id, description, deadline) VALUES($1, $2, $3) RETURNING id;"
+	QuerieCreate            = "INSERT INTO tasks (project_id, description, deadline) VALUES($1, $2, $3) RETURNING id;"
+	QuerieUpdateDescription = "UPDATE tasks SET description = $1 WHERE id = $2;"
+	QuerieUpdateDeadline    = "UPDATE tasks SET deadline = $1 WHERE id = $2;"
 )
