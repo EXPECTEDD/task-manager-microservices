4c916fb5de3e24e4e5eb6cd24aca96ada1d14517
EXPECTEDD <maksimknutov.w80@gmail.com>
2026-03-20 02:51:44 +0300

    feat: added implementations for change description and change deadline funcs

--- taskservice/internal/infrastructure/postgres/postgres.go
+++ taskservice/internal/infrastructure/postgres/postgres.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	taskdomain "taskservice/internal/domain/task"
 	posmapper "taskservice/internal/infrastructure/postgres/mapper"
+	"taskservice/internal/repository/storage"
+	"time"
 )
 
 var (
@@ -34,4 +36,38 @@ func (p *Postgres) Save(ctx context.Context, td *taskdomain.TaskDomain) (uint32,
 	}
 
 	return id, nil
+}
+
+func (p *Postgres) ChangeDescription(ctx context.Context, taskId uint32, newDescription string) error {
+	res, err := p.db.ExecContext(ctx, QuerieUpdateDescription, newDescription, taskId)
+	if err != nil {
+		return err
+	}
+
+	ra, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if ra == 0 {
+		return storage.ErrTaskNotFound
+	}
+
+	return nil
+}
+
+func (p *Postgres) ChangeDeadline(ctx context.Context, taskId uint32, newDeadline time.Time) error {
+	res, err := p.db.ExecContext(ctx, QuerieUpdateDeadline, newDeadline, taskId)
+	if err != nil {
+		return err
+	}
+
+	ra, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if ra == 0 {
+		return storage.ErrTaskNotFound
+	}
+
+	return nil
 }
