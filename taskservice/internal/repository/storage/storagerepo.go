e32b00ec8a9ea168e6eec8d06564542ec9373237
EXPECTEDD <maksimknutov.w80@gmail.com>
2026-03-20 02:51:05 +0300

    chore: added new funcs

--- taskservice/internal/repository/storage/storagerepo.go
+++ taskservice/internal/repository/storage/storagerepo.go
@@ -3,8 +3,11 @@ package storage
 import (
 	"context"
 	taskdomain "taskservice/internal/domain/task"
+	"time"
 )
 
 type StorageRepo interface {
 	Save(ctx context.Context, td *taskdomain.TaskDomain) (uint32, error)
+	ChangeDescription(ctx context.Context, taskId uint32, newDescription string) error
+	ChangeDeadline(ctx context.Context, taskId uint32, newDeadline time.Time) error
 }
