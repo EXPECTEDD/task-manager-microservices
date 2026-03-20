ce55f951d5d9e8dc5c18fdbc8965de4bac847c47
EXPECTEDD <maksimknutov.w80@gmail.com>
2026-03-20 19:46:40 +0300

    chore: added check project id

--- taskservice/internal/infrastructure/postgres/queries.go
+++ taskservice/internal/infrastructure/postgres/queries.go
@@ -2,6 +2,6 @@ package postgres
 
 var (
 	QuerieCreate            = "INSERT INTO tasks (project_id, description, deadline) VALUES($1, $2, $3) RETURNING id;"
-	QuerieUpdateDescription = "UPDATE tasks SET description = $1 WHERE id = $2;"
-	QuerieUpdateDeadline    = "UPDATE tasks SET deadline = $1 WHERE id = $2;"
+	QuerieUpdateDescription = "UPDATE tasks SET description = $1 WHERE id = $2 AND project_id = $3;"
+	QuerieUpdateDeadline    = "UPDATE tasks SET deadline = $1 WHERE id = $2 AND project_id = $3;"
 )
