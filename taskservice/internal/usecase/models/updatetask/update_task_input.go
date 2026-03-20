504c4342383e6aa09d739eb209cf3060ccc701ee
EXPECTEDD <maksimknutov.w80@gmail.com>
2026-03-20 19:49:01 +0300

    chore: added project id

--- taskservice/internal/usecase/models/updatetask/update_task_input.go
+++ taskservice/internal/usecase/models/updatetask/update_task_input.go
@@ -4,13 +4,16 @@ import "time"
 
 type UpdateTaskInput struct {
 	TaskId         uint32
+	ProjectId      uint32
 	NewDescription *string
 	NewDeadline    *time.Time
 }
 
-func NewUpdateTaskInput(taskId uint32, newDescription *string, newDeadline *time.Time) *UpdateTaskInput {
+func NewUpdateTaskInput(taskId uint32, projectId uint32, newDescription *string, newDeadline *time.Time) *UpdateTaskInput {
 	return &UpdateTaskInput{
 		TaskId:         taskId,
+		ProjectId:      projectId,
 		NewDescription: newDescription,
+		NewDeadline:    newDeadline,
 	}
 }
