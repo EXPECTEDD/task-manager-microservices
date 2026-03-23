93d9ead98a198502a1a46db923e49e37964e2eb3
EXPECTEDD <maksimknutov.w80@gmail.com>
2026-03-23 15:44:21 +0300

    chore: added delete querie

--- taskservice/internal/infrastructure/postgres/queries.go
+++ taskservice/internal/infrastructure/postgres/queries.go
@@ -4,4 +4,5 @@ var (
 	QuerieCreate            = "INSERT INTO tasks (project_id, description, deadline) VALUES($1, $2, $3) RETURNING id;"
 	QuerieUpdateDescription = "UPDATE tasks SET description = $1 WHERE id = $2 AND project_id = $3;"
 	QuerieUpdateDeadline    = "UPDATE tasks SET deadline = $1 WHERE id = $2 AND project_id = $3;"
+	QuerieDelete            = "DELETE FROM tasks WHERE id = $1 AND project_id = $2;"
 )
