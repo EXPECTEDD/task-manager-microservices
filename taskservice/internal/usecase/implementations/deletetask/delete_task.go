4e71fe90902ce856730af32d7a5b3f4e12c14dbc
EXPECTEDD <maksimknutov.w80@gmail.com>
2026-03-23 15:58:11 +0300

    feat: added implementation for delete usecase

---
+++ taskservice/internal/usecase/implementations/deletetask/delete_task.go
@@ -0,0 +1,44 @@
+package deletetask
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"taskservice/internal/repository/storage"
+	deleteerr "taskservice/internal/usecase/error/deletetask"
+	deletemodel "taskservice/internal/usecase/models/deletetask"
+)
+
+type DeleteUC struct {
+	log  *slog.Logger
+	stor storage.StorageRepo
+}
+
+func NewDeleteUC(log *slog.Logger, stor storage.StorageRepo) *DeleteUC {
+	return &DeleteUC{
+		log:  log,
+		stor: stor,
+	}
+}
+
+func (d *DeleteUC) Execute(ctx context.Context, in *deletemodel.DeleteTaskInput) (*deletemodel.DeleteTaskOutput, error) {
+	const op = "deletetask.Execute"
+
+	log := d.log.With(slog.String("op", op))
+
+	log.Info("starting deleting task")
+
+	err := d.stor.Delete(ctx, in.TaskId, in.ProjectId)
+	if err != nil {
+		if errors.Is(err, storage.ErrTaskNotFound) {
+			log.Info("task not found")
+			return deletemodel.NewDeleteTaskOutput(false), deleteerr.ErrTaskNotFound
+		}
+		log.Warn("cannot delete task", slog.String("error", err.Error()))
+		return deletemodel.NewDeleteTaskOutput(false), err
+	}
+
+	log.Info("task deletion completed successfully")
+
+	return deletemodel.NewDeleteTaskOutput(true), nil
+}
